c2cff3cadb41c47f7c8db3d5ab3a37345b4ba77c
Everton Reis <joaoeverton2015@gmail.com>
2024-08-21 16:55:04 -0300

    feat: migração da tabela media_files

---
+++ database/migrations/2024_08_21_191940_create_media_files_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('media_files', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('uploaded_by');
+            $table->string('file_name');
+            $table->string('file_path');
+            $table->string('mime_type');
+            $table->text('description')->nullable();
+            $table->bigInteger('file_size');
+            $table->boolean('is_visible')->default(true);
+            $table->boolean('is_dowloadable')->default(true);
+            $table->timestamps();
+
+            $table->foreign('uploaded_by')->references('id')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('media_files');
+    }
+};
