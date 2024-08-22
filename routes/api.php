c3a1cbf54afffa67ffa995a0dad0c558d1e0b5de
Everton Reis <joaoeverton2015@gmail.com>
2024-08-22 15:38:33 -0300

    refactor: remocão do sufixo upload

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::prefix('v1')->group(function () {
     Route::get('arquivos', [MediaFileController::class, 'index']);
-    Route::post('arquivos/upload', [MediaFileController::class, 'store']);
+    Route::post('arquivos', [MediaFileController::class, 'store']);
     Route::get('arquivos/{media_file}', [MediaFileController::class, 'show']);
     Route::put('arquivos/{media_file}', [MediaFileController::class, 'update']);
     Route::delete('arquivos/{media_file}', [MediaFileController::class, 'destroy']);
